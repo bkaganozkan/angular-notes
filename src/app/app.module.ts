a477dbcd31daf35311c50621bdb13c5654a418cb
Burak Kagan Ozkan <ozkankaan1996@hotmail.com>
2022-12-31 16:42:38 +0300

    fisrt section is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { DangerAlertComponent } from './danger-alert/danger-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, SuccessAlertComponent, DangerAlertComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
