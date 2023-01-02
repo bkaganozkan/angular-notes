42b305466e7ede9e65eb1b0d4ed84b7f20de229f
Burak Kagan Ozkan <ozkankaan1996@hotmail.com>
2023-01-02 21:17:39 +0300

    example basic project  initilazed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// * Custom Componenets
+import { HeaderComponenet } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+// * Custom Componenets End
+
+const COMPONTENTS = [
+  HeaderComponenet,
+  RecipesComponent,
+  RecipeListComponent,
+  RecipeDetailComponent,
+  RecipeItemComponent,
+  ShoppingListComponent,
+  ShoppingEditComponent,
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ...COMPONTENTS],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
